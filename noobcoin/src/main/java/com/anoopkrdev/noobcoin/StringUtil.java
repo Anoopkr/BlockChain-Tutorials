b7f630ceca95eaa7fe21e2a3cf79b577c1937d60
Anoop Kumar <anoopply2009@gmail.com>
2018-08-10 12:34:22 +0530

    Wallet to wallet transaction implementation

--- noobcoin/src/main/java/com/anoopkrdev/noobcoin/StringUtil.java
+++ noobcoin/src/main/java/com/anoopkrdev/noobcoin/StringUtil.java
@@ -5,6 +5,7 @@ import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class StringUtil {
@@ -58,5 +59,31 @@ public class StringUtil {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	// Tacks in array of transactions and returns a merkle root.
+	public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+		int count = transactions.size();
+		ArrayList<String> previousTreeLayer = new ArrayList<String>();
+		for (Transaction transaction : transactions) {
+			previousTreeLayer.add(transaction.transactionId);
+		}
+		ArrayList<String> treeLayer = previousTreeLayer;
+		while (count > 1) {
+			treeLayer = new ArrayList<String>();
+			for (int i = 1; i < previousTreeLayer.size(); i++) {
+				treeLayer.add(applySha256(previousTreeLayer.get(i - 1) + previousTreeLayer.get(i)));
+			}
+			count = treeLayer.size();
+			previousTreeLayer = treeLayer;
+		}
+		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+		return merkleRoot;
+	}
+
+	// Returns difficulty string target, to compare to hash. eg difficulty of 5 will
+	// return "00000"
+	public static String getDificultyString(int difficulty) {
+		return new String(new char[difficulty]).replace('\0', '0');
 	}
 }
